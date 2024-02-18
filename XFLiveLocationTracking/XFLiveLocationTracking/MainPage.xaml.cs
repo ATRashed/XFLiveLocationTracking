caee94c9526278be643ea83f7607af8acbcf368f
ATRashed <mri@accentechltd.com>
2024-02-18 18:09:55 +0600

    Initial commit

---
+++ XFLiveLocationTracking/XFLiveLocationTracking/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TcxTools;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using XFMapsSample;
+
+namespace XFLiveLocationTracking
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly Trackpoint[] pointsRoute1;
+        private readonly Trackpoint[] pointsRoute2;
+
+        private Pin pinRoute1 = new Pin
+        {
+            Label = "Other users 1"
+        };
+
+        private Pin pinRoute2 = new Pin
+        {
+            Label = "Other users 2"
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            pointsRoute1 = GetTrackingRoute("route.tcx");
+            pointsRoute2 = GetTrackingRoute("route1.tcx");
+
+            myMap.Pins.Add(pinRoute1);
+            myMap.Pins.Add(pinRoute2);
+            var index = 0;
+
+            Xamarin.Forms.Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                pinRoute1.Position = new Xamarin.Forms.Maps.Position(pointsRoute1[index].Position.LatitudeDegrees, pointsRoute1[index].Position.LongitudeDegrees);
+
+                pinRoute2.Position = new Xamarin.Forms.Maps.Position(pointsRoute2[index].Position.LatitudeDegrees, pointsRoute2[index].Position.LongitudeDegrees);
+
+                if (index == 0)
+                {
+                    myMap.MoveToRegion(MapSpan.FromCenterAndRadius(pinRoute1.Position, Xamarin.Forms.Maps.Distance.FromKilometers(1)));
+                }
+
+                index += 5;
+
+                return pointsRoute1.Length >= index;
+            });
+        }
+
+        private Trackpoint[] GetTrackingRoute(string filename)
+        {
+            using (var stream = Embedded.Load(filename))
+            {
+                var serializer = new XmlSerializer(typeof(TrainingCenterDatabase));
+                var @object = serializer.Deserialize(stream);
+
+                var database = (TrainingCenterDatabase)@object;
+                return database.Activities.Activity[0].Lap[0].Track.ToArray();
+            }
+        }
+    }
+}
